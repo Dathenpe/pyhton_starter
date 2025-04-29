81beeb0b27bf5224a41332b4ccef9117bdc34aee
Dathenpe <adetayoadedamola6@gmail.com>
2025-04-29 11:17:28 +0100

    bull

--- Section1/main.py
+++ Section1/main.py
@@ -5,14 +5,15 @@ print(f"multiplication of  {num1}  and   {num2}  is + {str(int(num1) * int(num2)
 print(f"subtraction of  {num1} and {num2} is {str(int(num1) - int(num2))}")
 print(f"division of  {num1} and { num2 } is{str(int(num1) / int(num2))}")
 
-name = 'shubham'
-age = 90
+name = input("enter your name")
+age = input("enter your age")
 print(f"my name is {name}")
 print(f"my age is {age}")
 print([22, 33, 44, 55, 66, 77, 88, 99])
 num2 = [22, 33, 44, 55, 66, 77, 88, 99]
 print(f"the total number of list is {len(num2)}")
 print(f"Hello, {name}!")
+print(f"Hello, {name}! You are {age} years old.")
 
 if age >= 18:
     print("You are an adult.")
