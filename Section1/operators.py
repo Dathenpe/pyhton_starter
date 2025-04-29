cea5274eb3513ae702ca31678c5b78712ced3504
Dathenpe <adetayoadedamola6@gmail.com>
2025-04-29 13:20:39 +0100

    bull

---
+++ Section1/operators.py
@@ -0,0 +1,26 @@
+#all arithmetic operators
+num1 =input("input a number")
+num2 =int(input(" input another number"))
+num1 = int(num1)
+sum = num1 + num2
+subtract = num1 - num2
+divide = num1/num2
+multiply = num1 * num2
+modulus = num1 % num2
+exponent = num1 ** num2
+
+print(f"addition of {num1} and {num2} = {sum}")
+print(f"subtraction of {num1} and {num2} = {subtract}")
+print(f"division of {num1} and {num2} = {divide}")
+print(f"multiplication of {num1} and {num2} = {multiply}")
+print(f"modulus of {num1} and {num2} = {modulus}")
+print(f"exponent of {num1} and {num2} = {exponent}")
+
+#relational operators
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
