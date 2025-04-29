acda63045c528dda2d8f0f41138e7c64c5245557
Dathenpe <adetayoadedamola6@gmail.com>
2025-04-29 10:45:20 +0100

    hey

---
+++ Section1/main.py
@@ -0,0 +1,19 @@
+num1 = input("enter the first number")
+num2 = input("enter the second number")
+print(f"addition of {num1} and  {num2}  is {str(int(num1) + int(num2))}")
+print(f"multiplication of  {num1}  and   {num2}  is + {str(int(num1) * int(num2))}")
+print(f"subtraction of  {num1} and {num2} is {str(int(num1) - int(num2))}")
+print(f"division of  {num1} and { num2 } is{str(int(num1) / int(num2))}")
+
+name = 'shubham'
+age = 90
+print(f"my name is {name}")
+print(f"my age is {age}")
+print([22, 33, 44, 55, 66, 77, 88, 99])
+num2 = [22, 33, 44, 55, 66, 77, 88, 99]
+print(f"the total number of list is {len(num2)}")
+print(f"Hello, {name}!")
+
+name =input('Enter you name')
+age =input('Enter your age')
+isadult_ =input('are you an adult?')
