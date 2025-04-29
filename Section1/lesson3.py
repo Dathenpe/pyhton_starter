7d0aa7ef9a9b78837093ec9b3ec48dd747c15770
Dathenpe <adetayoadedamola6@gmail.com>
2025-04-29 12:49:32 +0100

    changed

--- Section1/lesson3.py
+++ Section1/lesson3.py
@@ -1,3 +1,20 @@
 print("Hello World")
-name = input("enter your name as a string")
-print(f"Your are welcome to python programming {name}")
+
+
+#character description game
+fancy_line = "<----------------(❁´◡`❁)------------------>"
+name = input("enter your name")
+print(fancy_line)
+age = input("enter your age")
+print(fancy_line)
+bestFood = input("enter your best food")
+print(fancy_line)
+print(f"Your are welcome to python programming character game {name}")
+footballTeam = input("enter your favorite football team")
+if footballTeam is not None:
+    print(f"your favorite football team is {footballTeam},they are losers")
+print(fancy_line)
+townOfBirth = input("enter your town of birth")
+gender = input("enter your gender")
+print(f"this is your character sheet your name is {name}\n you are {age} years old\n you are {gender}\n you were born in {townOfBirth}\n your best food is {bestFood}\n and you unfortunately support {footballTeam}")
+print(fancy_line)
