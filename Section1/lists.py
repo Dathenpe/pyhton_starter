a1afffbc4d0dc426785aed7285df09a10fb88e49
Dathenpe <adetayoadedamola6@gmail.com>
2025-05-01 10:07:16 +0100

    bull

---
+++ Section1/lists.py
@@ -0,0 +1,27 @@
+#python lists
+
+list = [22, 33, 44, 55, 66, 77, 88, 99]
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[-1])
+print(list[-2])
+#mixed list
+list = [22, 33, 44, 55, 66, 77, 88, 99, "hello", "world"]
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[-1])
+print(list[-2])
