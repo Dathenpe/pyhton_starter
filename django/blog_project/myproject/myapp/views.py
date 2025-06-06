1e755aea8dbf1c2f013cb2c47423c7f1279165b0
Dathenpe <adetayoadedamola6@gmail.com>
2025-06-06 11:28:45 +0100

    feat(myapp): create basic blog functionality
    - Add Post model form for creating new blog posts
    - Create home view and template to display blog posts
    - Set up URL patterns for home view and new post creation
    - Update project URLs to include myapp URLs
    - Add create_post view for handling new post creation

--- django/blog_project/myproject/myapp/views.py
+++ django/blog_project/myproject/myapp/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .form import PostForm
 from .models import Post
 
 
 def home(request):
-    posts =Post.objects.all
-    return render(request, 'blog/home.html', {'posts': posts})
+    posts =Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
 
+def create_post(request):
+    if request.method == 'POST':
+       form = PostForm(request.POST)
+       if form.is_valid():
+           form.save()
+       else:
+           form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
 
 # Create your views here.
